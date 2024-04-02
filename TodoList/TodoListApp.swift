68b23dc12672daf521faf8ba3dc5f3e5cbdd1d88
Pavel Markitantov <markitantow@gmail.com>
2024-04-02 16:40:54 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Pavel Markitantov on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
