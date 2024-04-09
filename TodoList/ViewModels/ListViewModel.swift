cfecb498f5480dcbf16937822864aa35d965f2e4
Pavel Markitantov <markitantov@mail.com>
2024-04-09 20:37:22 +0200

    feat: add UserDefaults to save users todo list

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -8,19 +8,25 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title!", isCompleted: false),
-            ItemModel(title: "This is the second!", isCompleted: false),
-            ItemModel(title: "Third", isCompleted: true)
-        ]
-        items.append(contentsOf: newItems)
+        guard
+            let data       = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -39,6 +45,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -11,23 +11,37 @@ struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
 
     var body: some View {
-        List {
-            ForEach(listViewModel.items) { item in
-                ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
-                        }
+        Group {
+            if listViewModel.items.isEmpty {
+                VStack(alignment: .center, spacing: 8) {
+                    Text("Your list is empty")
+                        .fontWeight(.semibold)
+
+                    Text("Tap Add to insert your first todo item")
+                        .foregroundColor(.secondary)
+                }
+                .padding()
+                .multilineTextAlignment(.center)
+
+            } else {
+                List {
+                    ForEach(listViewModel.items) { item in
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
+                .listStyle(.plain)
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
         }
         .navigationTitle("Todo List")
         .navigationBarItems(leading: EditButton(),
                             trailing: NavigationLink("Add", destination: AddView()))
-
-        .listStyle(.plain)
     }
 }
 
